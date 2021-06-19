4f04b7b3673748b44529b54afb2c31f99fbca4f9
Youcef <youce.31@gmail.com>
2021-06-19 21:37:56 +0200

    change db to externe

--- util/database.js
+++ util/database.js
@@ -5,8 +5,8 @@ var sequelize = new Sequelize(
 );*/
 const sequelize = new Sequelize('Coordinator', 'postgres', 'postgres', {
   // host: 'postgres',  // name of the container
-  //host: '68.183.56.192',  // name of the container
-  host: 'localhost',  // name of the container
+  host: '68.183.56.192',  // name of the container
+  //host: 'localhost',  // name of the container
   dialect: 'postgres' 
 });
 module.exports = sequelize;
